83fcdf311a24b6cd4e0fecc97801ae1c98f6813f
satoshi-sh <satoss1108@gmail.com>
2023-05-09 18:00:47 -0500

    Add print to test

--- toot.py
+++ toot.py
@@ -37,8 +37,7 @@ def prepare_quiz():
 
 def make_toot(category,level,question,answers,tags,correct):
     time=600
-    print(os.environ.get('ACCESS_TOKEN'))
-    print(os.environ.get("URL"))
+    print(os.environ)
     mastodon = Mastodon(
         access_token=os.environ.get('ACCESS_TOKEN'),
         api_base_url = os.environ.get('URL'))
